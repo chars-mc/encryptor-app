fa1d797d68be51af4f3c10b32a7e947d9b062f1d
Carlos Manuel <chars.dev@gmail.com>
2021-11-11 20:09:41 -0600

    Add AppHeader and Spinner components

---
+++ src/shared/components/AppHeader.svelte
@@ -0,0 +1,17 @@
+<header>
+	<div class="container">
+		<h1>
+			<i class="bx bx-network-chart"></i>
+			ENCRYPTOR APP
+		</h1>
+	</div>
+</header>
+
+<style>
+	header {
+		padding: var(--padding);
+	}
+	h1 {
+		text-align: center;
+	}
+</style>
