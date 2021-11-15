9a2dd9373d6e989a4763105dd84c5a3b44f82914
Carlos Manuel <chars.dev@gmail.com>
2021-11-14 22:12:11 -0600

    Add CardInfo component

---
+++ src/shared/components/CardInfo.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let type: "SUCCESS" | "WARNING" | "DANGER";
+	export let message: string;
+</script>
+
+<template>
+	<div class="card card-{type.toLowerCase()}">
+		{message}
+	</div>
+</template>
+
+<style>
+	.card {
+		max-width: fit-content;
+		margin-left: auto;
+		margin-right: auto;
+		padding: var(--padding);
+		text-align: center;
+	}
+
+	.card-success {
+		color: var(--success-color);
+	}
+
+	.card-danger {
+		color: var(--danger-color);
+	}
+</style>
