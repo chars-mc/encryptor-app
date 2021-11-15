18008b9ec3d6a3ef76e04338aa46d4b8d4075713
Carlos Manuel <chars.dev@gmail.com>
2021-11-14 22:20:05 -0600

    Add UserLoginModel and verify fields

--- src/user/Login.svelte
+++ src/user/Login.svelte
@@ -1,13 +1,50 @@
 <script lang="ts">
+	import CardInfo from "../shared/components/CardInfo.svelte";
+	import { UserLogin } from "./domain/user";
+
+	let isLoading = false;
+	let error = "";
+	const userLogin = new UserLogin("", "");
+
+	function handleClick() {
+		isLoading = true;
+		try {
+			if (userLogin.verify()) {
+				console.log("correct");
+			}
+		} catch (err) {
+			error = err.message;
+		} finally {
+			isLoading = false;
+		}
+	}
 </script>
 
-<template>
+<div class="container">
 	<form method="post" class="login-form">
 		<h1>Login</h1>
 
-		<input type="text" placeholder="Username" />
-		<input type="password" placeholder="Password" />
+		<input
+			type="text"
+			placeholder="Username"
+			bind:value="{userLogin.username}"
+		/>
+		<input
+			type="password"
+			placeholder="Password"
+			bind:value="{userLogin.password}"
+		/>
 
-		<button class="btn-success">Login </button>
+		<button
+			class="btn-success"
+			type="submit"
+			disabled="{isLoading}"
+			on:click|preventDefault="{handleClick}"
+		>
+			Login
+		</button>
 	</form>
-</template>
+	{#if error}
+		<CardInfo type="DANGER" message="{error}" />
+	{/if}
+</div>
---
+++ src/user/domain/user.ts
@@ -0,0 +1,15 @@
+export class UserLogin {
+	constructor(public username: string, public password: string) {}
+
+	public verify(): boolean {
+		if (this.username === "" || this.password === "") {
+			throw new Error("The username and password cannot be empty");
+		}
+		if (this.username.length <= 10 || this.username.length > 30) {
+			throw new Error(
+				"The username and password length must be between 10 and 30 characters",
+			);
+		}
+		return true;
+	}
+}
