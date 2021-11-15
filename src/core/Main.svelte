4c4b18cafdeab440245f1a012d1a0d40563d32c7
Carlos Manuel <chars.dev@gmail.com>
2021-11-15 14:41:55 -0600

    Add base form

--- src/core/Main.svelte
+++ src/core/Main.svelte
@@ -1,15 +1,69 @@
 <script lang="ts">
-	import CardInfo from "../shared/components/CardInfo.svelte";
-
 	export let userRole: string;
+	let dataType = "text";
 </script>
 
 <template>
-	{#if userRole == "Encryptor"}
-		<h1>Encryptor</h1>
-	{:else if userRole == "Decryptor"}
-		<h1>Decryptor</h1>
-	{:else}
-		<CardInfo type="DANGER" message="User role unknown" />
-	{/if}
+	<form>
+		<div>
+			<input
+				type="radio"
+				name="data_type"
+				value="text"
+				bind:group="{dataType}"
+				id="text"
+			/>
+			<label for="text">Text</label>
+
+			<input
+				type="radio"
+				name="data_type"
+				value="file"
+				bind:group="{dataType}"
+				id="file"
+			/>
+			<label for="file">File</label>
+		</div>
+
+		{#if dataType === "text"}
+			<input type="text" placeholder="Enter text here" />
+		{:else}
+			<input type="file" placeholder="Select a file" />
+		{/if}
+
+		<button class="btn btn-success" type="submit">
+			{userRole === "Encryptor" ? "Encrypt" : "Decrypt"}
+		</button>
+	</form>
+
+	<p class="result">Result</p>
 </template>
+
+<style scoped>
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	input[type="text"],
+	input[type="file"],
+	button {
+		display: block;
+		width: 100%;
+	}
+	label {
+		display: inline-block;
+		margin-right: var(--padding);
+	}
+
+	.result {
+		margin: var(--padding);
+		padding: var(--padding);
+		border-radius: var(--border-radius);
+		height: 10rem;
+		background-color: rgba(0, 0, 0, 0.05);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
