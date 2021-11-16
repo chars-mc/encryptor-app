d1c66294d2e00349702882df2d6d761f63ff64ab
Carlos Manuel <chars.dev@gmail.com>
2021-11-16 13:52:45 -0600

    Add signup form

--- src/user/SignUp.svelte
+++ src/user/SignUp.svelte
@@ -3,11 +3,43 @@
 </script>
 
 <template>
-	<form>
+	<form class="form-auth">
 		<h1>SignUp</h1>
+		<input type="text" placeholder="Username" />
+		<input type="password" placeholder="Password" />
+		<input type="password" placeholder="Confirm password" />
+
+		<div>
+			<span>Role:</span>
+			<input type="radio" name="data_type" value="encryptor" id="text" />
+			<label for="text">Encryptor</label>
+			<input type="radio" name="data_type" value="decryptor" id="decryptor" />
+			<label for="file">Decryptor</label>
+		</div>
+
+		<button class="btn-success" type="submit"> Signup </button>
 
 		<p>
 			You already have an account? <Link to="/login">Login</Link>
 		</p>
 	</form>
 </template>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	input[type="text"],
+	input[type="password"],
+	button {
+		display: block;
+		width: 100%;
+	}
+
+	input[type="radio"],
+	label {
+		display: inline;
+	}
+</style>
